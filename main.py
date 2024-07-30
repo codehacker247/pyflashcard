00c87f8adbe80221096bdc967bb15bae02842ac3
Rohit <rohitkumarmandal34@gmail.com>
2024-07-30 15:41:12 +0530

    Added exception handling and added last message.

--- main.py
+++ main.py
@@ -8,22 +8,26 @@ to_learn = {}
 
 try:
     data = pandas.read_csv("data/words_to_learn.csv")
-except FileNotFoundError:
+except:
     original_data = pandas.read_csv("data/french_words.csv")
-    print(original_data)
     to_learn = original_data.to_dict(orient="records")
 else:
     to_learn = data.to_dict(orient="records")
 
 
 def next_card():
-    global current_card, flip_timer
-    window.after_cancel(flip_timer)
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
-    canvas.itemconfig(card_background, image=card_front_img)
-    flip_timer = window.after(3000, func=flip_card)
+    try:
+        global current_card, flip_timer
+        window.after_cancel(flip_timer)
+        current_card = random.choice(to_learn)
+    except IndexError:
+        canvas.itemconfig(card_title, text="Completed", fill="black")
+        canvas.itemconfig(card_word, text="Run again", fill="black", font=("Ariel", 60, "bold"))
+    else:
+        canvas.itemconfig(card_title, text="French", fill="black")
+        canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+        canvas.itemconfig(card_background, image=card_front_img)
+        flip_timer = window.after(5000, func=flip_card)
 
 
 def flip_card():
@@ -33,18 +37,23 @@ def flip_card():
 
 
 def is_known():
-    to_learn.remove(current_card)
-    print(len(to_learn))
-    data = pandas.DataFrame(to_learn)
-    data.to_csv("data/words_to_learn.csv", index=False)
-    next_card()
+    try:
+        to_learn.remove(current_card)
+        print(len(to_learn))
+        new_data = pandas.DataFrame(to_learn)
+        new_data.to_csv("data/words_to_learn.csv", index=False)
+    except IndexError:
+        canvas.itemconfig(card_title, text="Completed", fill="black")
+        canvas.itemconfig(card_word, text="Run again", fill="black", font=("Ariel", 60, "bold"))
+    else:
+        next_card()
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer = window.after(3000, func=flip_card)
+flip_timer = window.after(5000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
